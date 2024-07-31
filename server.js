0b69c00409efc7c04554552a89235fade6d2f9bf
Saksham <sakshamm9399@gmail.com>
2024-07-31 17:40:58 +0530

    added

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+const user = require("./utility/user.json");
+const path = require("path");
+const fileURLtoPath = require("url");
+
+
+
+const PORT = 5000;
+
+
+const app = express();
+app.use(express.json());
+
+//static files wala middleware
+app.use(express.static(path.resolve(__dirname, "./Public")));
+
+// properties of express has been transfered into app
+app.use(express.json());
+
+app.get('/user', (req, res) => {
+    res.send(user);
+
+});
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./Public", "index.html"));
+})
+
+app.get("/contact", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./Public", "contact.html"));
+})
+
+app.listen(PORT, () => {
+console.log(`server is running on port ${PORT}`.green);
+});
